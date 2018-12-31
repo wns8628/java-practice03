bafc51ea444f4b446d3b0dfbd7a6af312a9de33b
maimai <maimaitoon@inbox.lv>
2018-12-31 20:31:21 +0900

    끝

--- src/prob6/CalcApp.java
+++ src/prob6/CalcApp.java
@@ -14,28 +14,35 @@ public class CalcApp {
 
 			String[] cal_num = expression.split(" ");
 			
-
-			if(cal_num[1].equals("+")) {
-				Add add = new Add();
-				add.setValue(Integer.parseInt(cal_num[0]), Integer.parseInt(cal_num[2]));
-				System.out.println(add.calculate());
-			}
-			if(cal_num[1].equals("-")) {
-				Sub sub = new Sub();
-				sub.setValue(Integer.parseInt(cal_num[0]), Integer.parseInt(cal_num[2]));
-				System.out.println(sub.calculate());
-			}
-			if(cal_num[1].equals("*")) {
-				Add add = new Add();
-				add.setValue(Integer.parseInt(cal_num[0]), Integer.parseInt(cal_num[2]));
-				System.out.println(add.calculate());
-			}
-			if(cal_num[1].equals("/")) {
-				Add add = new Add();
-				add.setValue(Integer.parseInt(cal_num[0]), Integer.parseInt(cal_num[2]));
-				System.out.println(add.calculate());
+			if(expression.equals("quit")) {
+				break;
 			}
 			
+			switch(cal_num[1]) {
+				case "+":
+					Add add = new Add();
+					add.setValue(Integer.parseInt(cal_num[0]), Integer.parseInt(cal_num[2]));
+					System.out.println(add.calculate());
+					break;
+				case "-":
+					Sub sub = new Sub();
+					sub.setValue(Integer.parseInt(cal_num[0]), Integer.parseInt(cal_num[2]));
+					System.out.println(sub.calculate());
+					break;
+				case "*":
+					Mul mul = new Mul();
+					mul.setValue(Integer.parseInt(cal_num[0]), Integer.parseInt(cal_num[2]));
+					System.out.println(mul.calculate());
+					break;
+				case "/":
+					Div div = new Div();
+					div.setValue(Integer.parseInt(cal_num[0]), Integer.parseInt(cal_num[2]));
+					System.out.println(div.calculate());
+					break;
+				default :
+					System.out.println("제데로입력하라");
+					continue;
+			}
 			
 		}
 	}
